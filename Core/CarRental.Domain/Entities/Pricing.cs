2826cccface9752142a5fd9d570014f1aa59e993
Enes <onerenes12@gmail.com>
2025-12-21 17:16:12 +0300

    initial entity definitions

---
+++ Core/CarRental.Domain/Entities/Pricing.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.Domain.Entities
+{
+	public class Pricing
+	{
+		public int Id { get; set; }
+		public string Name { get; set; } //Günlük, haftalık, aylık
+		public List<CarPricing> CarPricings { get; set; }
+
+	}
+}
